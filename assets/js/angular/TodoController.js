9ee90388740c7f61af8c19fd89a2f6d9fe931866
Vaibhav Kohli <vaibhav_kohli@external.mckinsey.com>
2014-04-28 16:14:12 +0530

    socket.io integrated

--- assets/js/angular/TodoController.js
+++ assets/js/angular/TodoController.js
@@ -1,12 +1,26 @@
 myApp.controller('TodoController', function ($scope, $http, TodoService){
 	$scope.todoData = {};
-	TodoService.get()
-	    .success(function(data){
-	    	$scope.todos = data;
-	    })
-	    .error(function(err){
-	    	console.log(err);
-	    });
+	var socket = io.connect();
+    socket.on('connect', function socketConnected() {
+    	console.log('someone connected');
+    	socket.on('message', function messageReceived(message) {
+    		console.log('hello');
+    		$scope.todos.push(message.data);
+    		$scope.$apply();
+    	});
+    });
+	socket.get('/todo', function(data){
+		$scope.todos = data
+		$scope.$apply();
+	});
+
+	// TodoService.get()
+	//     .success(function(data){
+	//     	$scope.todos = data;
+	//     })
+	//     .error(function(err){
+	//     	console.log(err);
+	//     });
 	$scope.submitTodo = function(todoData) {
 		TodoService.store(todoData)
 		.success(function(data) {
